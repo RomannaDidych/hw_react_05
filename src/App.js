668ad65832d9da74e604620965cefb4042f316ad
unknown <romanna.d@gmail.com>
2021-01-26 23:44:17 +0200

    ready to test

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import './App.css';
 import React from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, Redirect, Link} from 'react-router-dom';
 import Signupform from './Components/Sign-Up-Form/Sign-up-form';
 import Signinform from './Components/Sign-In-form/Sign-in-form'
 
 function App() {
   return (
-    <div className="App">      
-      <Router>
-        <Link to='/'></Link>
-        <Link to='/sign-in-form'></Link>
-        <Route  exact path="/" component= {Signupform} />          
-        <Route  path="/sign-in-form" component= {Signinform} />          
+    <div className="App">
+    <Router>      
+      <Switch>
+        <Route  path="/sign-up-form" component= {Signupform} />
+        <Route  path="/sign-in-form" component= {Signinform} />
+        <Redirect to="/sign-up-form" />
+      </Switch>
       </Router>
     </div>
   );
 }
 
 export default App;
+
+
+
